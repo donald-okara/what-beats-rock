cae054a92ff0c8602883ae5bec53293b7c64d872
donald-okara <donaldokara123@gmail.com>
2025-08-02 14:07:10 +0300

    First release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ke.don.what_beats_rock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
